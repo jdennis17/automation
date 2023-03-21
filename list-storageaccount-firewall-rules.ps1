2ea74157330843d0c0510db882ca9a727c742132
James <jdennis@definitivelogic.com>
2023-03-21 11:45:07 -0400

    Storage Accounts and Firewall Rules

---
+++ list-storageaccount-firewall-rules.ps1
@@ -0,0 +1,18 @@
+﻿#Connect-AzAccount
+$Result=@()
+$Storageaccounts = Get-AzureRMStorageAccount
+$Storageaccounts | ForEach-Object {
+$storageaccount = $_
+Get-AzureRMStorageAccountNetworkRuleSet -ResourceGroupName $storageaccount.ResourceGroupName -AccountName $storageaccount.StorageAccountName | ForEach-Object {
+$Result += New-Object PSObject -property @{ 
+Account = $storageaccount.StorageAccountName
+ResourceGroup = $storageaccount.ResourceGroupName
+Bypass = $_.Bypass
+Action = $_.DefaultAction
+IPrules = $_.IpRules
+Vnetrules = $_.VirtualNetworkRules
+ResourceRules = $_.ResourceAccessRules
+}
+}
+}
+$Result | Select Account,ResourceGroup,Bypass,Action,IPrules,Vnetrules,ResourceRules
