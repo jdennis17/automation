4caeaec1c0fb64c14cd0f543027522f14820ccb2
jdennis17 <27358623+jdennis17@users.noreply.github.com>
2025-03-11 14:20:48 -0400

    Update list-storageaccounts-containers.ps1

--- list-storageaccounts-containers.ps1
+++ list-storageaccounts-containers.ps1
@@ -1,10 +1,10 @@
-﻿## Settings below pulled from Azure Portal and Ansible Server - /etc/profile.d/azure_service_principal.sh file
+
 $azureEnvironment    = "AzureUSGovernment"
 $azureLocation       = "USGov Virginia"
-$azureSubscriptionId = "77500eef-8657-4927-8c3b-68ef341a9af3"
-$azureTenantId       = "0ed52ee3-ab90-4e18-ac02-a60dcc265704"
-$azureClientId       = "40e86467-4ae4-40ad-ae63-aad2bf98d990"
-$azurePassword       = ConvertTo-SecureString "af33c19b-4d5f-49ec-a469-181efecbcfc0" -AsPlainText -Force
+$azureSubscriptionId = ""
+$azureTenantId       = ""
+$azureClientId       = ""
+$azurePassword       = ConvertTo-SecureString "" -AsPlainText -Force
 Write-Host "Logging in to Azure"
 $psCred = New-Object System.Management.Automation.PSCredential($azureClientId , $azurePassword)
 Connect-AzureRmAccount -Credential $psCred -TenantId $azureTenantId -EnvironmentName $azureEnvironment -ServicePrincipal
@@ -49,4 +49,4 @@ $storageAccounts = Get-AzureRmResource -ResourceType 'Microsoft.Storage/storageA
         $saUsage.add((New-Object psobject -Property $StorageAccountDetails)) | Out-Null
      }     
 }
-$saUsage | Export-Csv -Path C:\Users\JAMES.DENNIS\Desktop\storageaccounts\devlab+test-storage.csv -NoTypeInformation
+$saUsage | Export-Csv -Path C:\temp\file.csv -NoTypeInformation
